2b3bc8d7c1c130f7f2208544057f28fd0b71da7c
Simeon Bateman <simeon@simb.net>
2010-12-01 14:55:49 -0800

    reverse ordered the images on the profile page

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ server.get('/', function(req, res, params) {
 })
 
 server.get('/p/:username', function(req, res){
-	Picture.find({username: req.params.username }).all(function(pics){
+	Picture.find({username: req.params.username }).sort([['created_on', 'descending']]).all(function(pics){
         res.render('profile.ejs', {
 	        locals: {
 	            username: req.params.username,
